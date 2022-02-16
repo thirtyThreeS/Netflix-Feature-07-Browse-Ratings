8ea7557eb442c25234627bb30db076c5b1b59ac4
thirtyThreeS <thaumasios@outlook.com>
2022-02-16 13:10:35 -0500

    Add project files.

---
+++ Browse Ratings/MaxStack.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Browse_Ratings
+{
+    public class MaxStack
+    {
+        Stack<int> mainStack; // Original Values
+        Stack<int> maxStack;  // Max Value w/ Top of maxStack always being max value.
+
+        public MaxStack()
+        {
+            mainStack = new Stack<int>();
+            maxStack = new Stack<int>();
+        }
+
+        // Removes and returns value from stack
+        public int Poppers()
+        {
+            maxStack.Pop();         // Pop element from maxStack making it sync with mainStack
+            return mainStack.Pop(); // Pop element from mainStack and return it's value
+        }
+
+        // Pushes value into the stack
+        public void Pusher(int value)
+        {
+            mainStack.Push(value);
+            if (maxStack.Count != 0 && maxStack.Peek() > value) maxStack.Push(maxStack.Peek()); // Push value in mainStack and check value with the top value of maxStack 
+            else maxStack.Push(value);                                                          // If value is greater than top.. push to top in mainStack
+        }                                                                                       // Else push value in maxStack
+
+        // Returns max value in O(1)
+        public int MaxRating()
+        {
+            return maxStack.Peek(); 
+        }
+    }
+}
---
+++ Browse Ratings/Program.cs
@@ -0,0 +1,23 @@
+﻿using Browse_Ratings;
+
+class Solution
+{
+    public static void Main()
+    {
+        MaxStack stack = new();
+        stack.Pusher(5);
+        stack.Pusher(0);
+        stack.Pusher(2);
+        stack.Pusher(4);
+        stack.Pusher(6);
+        stack.Pusher(3);
+        stack.Pusher(10);
+
+        Console.WriteLine($"Maximum rating: {stack.MaxRating()}");
+
+        stack.Poppers();
+        Console.WriteLine("\nAfter clicking back button\n");
+
+        Console.WriteLine($"Maximum Rating: {stack.MaxRating()}");
+    }
+}
